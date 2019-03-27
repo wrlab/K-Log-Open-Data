f7822189ac57b7131dc4e623e4e682e279011df5
Lipnitskiy Alexander <lipnitskiy.alexander@gmail.com>
2019-03-27 16:34:23 +0900

    add graphql dependency

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
-
+const graphqlHTTP = require('express-graphql');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -45,6 +45,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/graphql', graphqlHTTP({
+  schema: {},
+  graphiql: true
+}));
 
 app.use('/td', indexRouter);
 app.use('/api/airquality', airQualityRouterApi);
